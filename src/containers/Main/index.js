78825c172ec2d4482640d3719a9fa40a673a8546
SerafimPoch <serpochapinsky@gmail.com>
2018-10-04 13:27:29 +0300

    add javascript & java section

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Introduction from "../../components/Main/introduction";
-import WhatYouShouldKnow from "../../components/Main/what_should_you_know";
 import { MainContainer } from "./style";
+import Introduction from "../../components/Main/introduction";
+import JavascriptJava from "../../components/Main/javascript_java";
+import WhatYouShouldKnow from "../../components/Main/what_should_you_know";
 
 export default () => {
   return (
     <MainContainer>
       <Introduction />
       <WhatYouShouldKnow />
+      <JavascriptJava />
     </MainContainer>
   );
 };

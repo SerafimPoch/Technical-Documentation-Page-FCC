4ae87e18a123ac53bbce86f20e64e104349fbc29
SerafimPoch <serpochapinsky@gmail.com>
2018-10-07 13:15:22 +0300

    add main mobile version

--- src/components/Main/introduction/style.js
+++ src/components/Main/introduction/style.js
@@ -6,4 +6,9 @@ export const Section = styled.section`
   align-items: flex-start;
   margin-bottom: 30px;
   width: 98%;
+
+  @media (min-width: 320px) and (max-width: 768px) {
+    width: 100%;
+    align-items: center;
+  }
 `;
--- src/containers/Main/style.js
+++ src/containers/Main/style.js
@@ -5,4 +5,9 @@ export const MainContainer = styled.div`
   flex-flow: column wrap;
   justify-content: space-between;
   margin-left: 310px;
+
+  @media (min-width: 320px) and (max-width: 768px) {
+    margin-left: 0px;
+    width: 100%;
+  }
 `;
